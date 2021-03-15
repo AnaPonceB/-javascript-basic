1d62e47077ecae6fadf45880f07cd933c67f2ba1
Ana Ponce <74144776+AnaPonceB@users.noreply.github.com>
2021-03-15 15:50:12 +0100

    Add files via upload
    exercicis js bàsic

---
+++ main.js
@@ -0,0 +1,56 @@
+
+//exercici 1
+console.log("Hola mundo");
+
+//exercici 2
+alert("Hola em dic Ana");
+
+//exercici 3
+var nom="Ana"; cognom="Ponce";
+var nomcomplet=nom+" "+cognom;
+console.log(nomcomplet)
+
+//exercici 4
+var x, y, total;
+x=34;
+y=56; 
+total=x+y;
+console.log("La suma de 34 i 56 és: " +total);
+
+//exercici 5
+
+var nota_examen=8;
+
+if (nota_examen > 5) {
+    alert("Has aprovat amb un " + nota_examen);
+}
+if (nota_examen < 5){
+    alert("Has suspès amb un " + nota_examen);
+}
+
+//exercici 6
+
+var frase="Tinc un cotxe de color"; color="verd"; paraula="cotxe";
+var fraseall=frase+" "+color;
+console.log (fraseall)
+
+var fraseall
+if (color="verd") {
+    console.log("Tinc un cotxe de color blau");
+}
+if (paraula="cotxe") {
+    console.log("Tinc un cutxe de culur blau")
+}
+
+//exercici 7 bucles
+
+var llistat=["taula", "cadira", "ordinador", "llibreta"];
+for(var i = 0; i < llistat.length; i++){
+    console.log ("L'objecte " + llistat[i] + " està en la posició " + i); 
+}
+
+
+
+
+
+
